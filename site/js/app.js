2ecd5f72fe24fbeb4c59ed62e3a337e074d23e78
mpmckenna8 <mpmckenna8@me.com>
2015-05-22 12:46:45 -0700

    changed page title and some otherstuff

--- site/js/app.js
+++ site/js/app.js
@@ -8,3 +8,26 @@ $(function() {
 
     Backbone.emulateHTTP = false;
 });
+
+console.log($("#addarea").css("display"))
+
+
+$("#abook").click(function(){
+  console.log('add bokk got clicked yo')
+
+  console.log($("#addarea").css("display"));
+
+  if( $("#addarea").css("display") == "none"){
+
+
+
+  $("#addarea")
+    .css("display", 'inline');
+  }
+    
+
+
+
+
+
+})
